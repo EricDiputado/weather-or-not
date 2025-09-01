5e2d0ce13290716fc91700cf1ef2a1689bb4ef95
Eric Diputado <ericdiputado@gmail.com>
2025-09-01 21:44:52 +1200

    Fixed the search functionality

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,16 +15,34 @@ namespace WeatherOrNot.Pages
             _weatherService = weatherService;
         }
 
+        private string? _city;
+
         [BindProperty(SupportsGet = true)]
-        public string? City { get; set; }
+        public string? City
+        {
+            get => _city;
+            set
+            {
+                _city = value;
+                if (!string.IsNullOrWhiteSpace(_city))
+                {
+                    // Reset weather data whenever City is set
+                    Weather = _weatherService.GetWeatherForCityAsync(_city).GetAwaiter().GetResult();
+                }
+                else
+                {
+                    Weather = null;
+                }
+            }
+        }
 
         public WeatherResponse? Weather { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (!string.IsNullOrWhiteSpace("Auckland"))
+            if (!string.IsNullOrWhiteSpace(City))
             {
-                Weather = await _weatherService.GetWeatherForCityAsync("Auckland");
+                Weather = await _weatherService.GetWeatherForCityAsync(City);
             }
         }
     }
